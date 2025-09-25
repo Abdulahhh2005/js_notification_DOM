7212ad3575cd4a8fca2346b071e29a3aaad84af4
Oleksiy Rozsokha <xanderiooo21@gmail.com>
2025-09-25 12:24:01 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,44 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  // Присвоєння або створення елементів
+  const body = document.querySelector('body');
+  const box = document.createElement('div');
+  const titleBox = document.createElement('h2');
+  const descriptionBox = document.createElement('p');
+
+  // Додавання класу
+  box.classList.add('notification');
+
+  // Фарбування box
+  if (type === 'success') {
+    box.classList.add('success');
+  } else if (type === 'warning') {
+    box.classList.add('warning');
+  } else {
+    box.classList.add('error');
+  }
+
+  // Додати клас зі стилями і присвоїти контент загаловку
+  titleBox.classList.add('title');
+  titleBox.textContent = title;
+
+  // Присвоїти контент опису
+  descriptionBox.textContent = description;
+
+  // Додати стилі позиціонування
+  box.style.top = posTop + 'px';
+  box.style.right = posRight + 'px';
+
+  // Метод який робить щось через 2 секунди
+  setTimeout(() => {
+    box.style.display = 'none';
+  }, 2000);
+
+  // Складання
+  box.append(titleBox);
+  box.append(descriptionBox);
+  body.append(box);
 };
 
 pushNotification(
